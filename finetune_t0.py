9f989676f744bdd414d681b6cdcdb5fed1113682
u4005115 <u4005115@ln01-twnia2.twcc.ai>
2023-03-30 14:42:27 +0800

    update

--- finetune_t0.py
+++ finetune_t0.py
@@ -43,6 +43,14 @@ def model_provider(pre_process=True, post_process=True):
             model._megatron_batch_fn = get_batch_pipe
         else:
             raise NotImplementedError("DeepSpeed is required for T0")
+        if args.enable_lora:
+            print("[LoRA] mark_only_lora_as_trainable")
+            for n, p in model.named_parameters():
+                if "lora_" not in n:
+                    print("[LoRA] non-trainable:", n)
+                    p.requires_grad = False
+                else:
+                    print("[LoRA] trainable:", n)
 
     see_memory_usage(f"After Building Model", force=True)
     return model
